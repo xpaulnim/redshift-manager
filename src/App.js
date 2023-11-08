05d86bbd6a52073d2a1939184b496767d29860bb
Paul Nimusiima <xpaulnim@users.noreply.github.com>
2023-11-08 02:22:47 +0000

    use files from react-app init
    npm init react-app my-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import React from "react";
-
-import { HeaderComponent } from './components/Header/HeaderComponent'
-
-export class App extends React.Component {
-    render(){
-        return (
-            <div>
-                <HeaderComponent/>
-            </div>
-        )
-    }
+function App() {
+    return (
+        <div>
+            <p>a paragraph</p>
+        </div>
+    )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from "react"
+import ReactDOM from "react-dom/client"
 
-import {App} from './App'
+import App from './App'
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
